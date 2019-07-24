a2f9d338adb5d5207b5a97b48e5327454669f1ce
Jeremy Morales <jeremy.morales@gmail.com>
2019-07-24 16:11:06 -0700

    get, put, and delete functional

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // implement your API here
+const express = require('express');
+const Users = require('./data/db.js');
+const server = express();
+
+server.use(express.json());
+
+server.get('/api/users', (req, res) => {
+  Users.find()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(err => {
+      req.status(500).json({ success: false, err });
+    });
+});
+
+server.post('/api/users', (req, res) => {
+  const { name, bio } = req.body;
+
+  if (!name || !bio) {
+    res.status(400).json({ errorMessage: 'Requires name and bio for user' });
+  } else {
+    Users.insert(req.body)
+      .then(user => {
+        res.status(201).json(user);
+      })
+      .catch(() => {
+        res.status(500).json({
+          errorMessage: 'Error saving user to database'
+        });
+      });
+  }
+});
+
+server.delete('/api/users/:id', (req, res) => {
+  Users.remove(req.params.id).then(deleted => {
+    if (deleted > 0) {
+      res.status(200).json({ message: 'User Deleted' });
+    } else {
+      res.status(404).json({ message: 'User does not exist' });
+    }
+  });
+});
+
+server.listen(4000, () => {
+  console.log('Server running on http://localhost:4000');
+});
